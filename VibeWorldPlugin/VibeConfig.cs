f0d048ee50b0eba7c8f1718bdd667d8ff3863070
HelloThere <119542492+SortaUnknown@users.noreply.github.com>
2022-12-01 00:46:15 +0100

    Remove unnecessary usings in VibeConfig.cs

--- VibeWorldPlugin/VibeConfig.cs
+++ VibeWorldPlugin/VibeConfig.cs
@@ -1,11 +1,5 @@
-﻿using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
-using Music;
-using RWCustom;
-using System.IO;
 using OptionalUI;
-using BepInEx;
 
 namespace VibeWorld
 {
